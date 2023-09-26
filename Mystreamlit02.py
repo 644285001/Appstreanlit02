7d06c119c62c87ef5ed88aabc3886c220f253ce6
chinnawat99 <119986113+chinnawat99@users.noreply.github.com>
2023-09-26 15:10:36 +0700

    .commit

--- Mystreamlit02.py
+++ Mystreamlit02.py
@@ -4,4 +4,18 @@ st.image('./pic/chinnawat.jpg')
 st.image('./pic/iris1.jpg')
 st.image('./pic/iris2.jpg')
 st.image('./pic/iris3.jpg')
-st.header('Chinnawat')
+
+col1, col2 = st.columns(2)
+with col1:
+    st.header('Chinnawat')
+with col2:
+    st.subheader('สาขาวิทยาการข้อมูล')
+    st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
+
+html_1 = """
+<div style="background-color:#76D7C4;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h4>การทำนายข้อมูลดอกไม้เบื้องต้น</h4></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
