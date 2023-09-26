ed01f53f71825994b8cb58059d5d980d565d6b41
chinnawat99 <119986113+chinnawat99@users.noreply.github.com>
2023-09-26 15:03:07 +0700

    .commit

--- Mystreamlit02.py
+++ Mystreamlit02.py
@@ -1,8 +1,12 @@
 import streamlit as st
 
 st.image('./pic/chinnawat.jpg')
-st.image('./pic/iris1.jpg')
-st.image('./pic/iris2.jpg')
-st.image('./pic/iris3.jpg')
+
+col1,col2=st.colums(2)
+col1 with:
+    st.header('ณัฐินันท์ เข็มทอง')
+col2 with:
+    st.subheader('สาขาวิทยาการข้อมูล')
+    st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
 
 st.header('Chinnawat')
