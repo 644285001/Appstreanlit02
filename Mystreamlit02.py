10a18fc916492b7e8cc1ea838d81fdaf7e2eff52
chinnawat99 <119986113+chinnawat99@users.noreply.github.com>
2023-09-26 14:59:33 +0700

    .commit

--- Mystreamlit02.py
+++ Mystreamlit02.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.image('/.pic/chinnawat.jpg')
-st.image('/.pic/iris1.jpg')
-st.image('/.pic/iris2.jpg')
-st.image('/.pic/iris3.jpg')
+st.image('./pic/chinnawat.jpg')
+st.image('./pic/iris1.jpg')
+st.image('./pic/iris2.jpg')
+st.image('./pic/iris3.jpg')
 
 st.header('Chinnawat')
